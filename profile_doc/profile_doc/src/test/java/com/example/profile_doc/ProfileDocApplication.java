ce8f611191ea49c307a8130b215c31360442be65
Vaijayanti-kamble <vaijayantinandkumar@gmail.com>
2025-02-10 16:23:17 +0530

    Add files via upload

---
+++ profile_doc/profile_doc/src/test/java/com/example/profile_doc/ProfileDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.profile_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileDocApplication.class, args);
+    }
+
+}
